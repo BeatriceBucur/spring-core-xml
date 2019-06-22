561c6b6902f10a734fb88fdc5b450b90edd98ac0
Beatrice <beatrice.bucur@gmail.com>
2019-06-22 13:59:17 +0300

    cu appConfig.java

--- src/main/java/sda/java5/springclass/Runner.java
+++ src/main/java/sda/java5/springclass/Runner.java
@@ -1,12 +1,17 @@
 package sda.java5.springclass;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sda.java5.springclass.config.AppConfig;
 import sda.java5.springclass.service.MessageSender;
 
 public class Runner {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("appContext.xml");
+        //ApplicationContext ctx = new ClassPathXmlApplicationContext("appContext.xml");
+        //daca nu lucram cu XML, va trebui sa incarcam contextul in felul de mai jos:
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         MessageSender sender = ctx.getBean(MessageSender.class);
         sender.sendMessage();
     }
---
+++ src/main/java/sda/java5/springclass/config/AppConfig.java
@@ -0,0 +1,13 @@
+package sda.java5.springclass.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan (basePackages = {"sda.java5.springclass"})
+
+public class AppConfig {
+
+
+
+
+}
